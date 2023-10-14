72eb3b8e1d28429915bf42b367c9c99e0c7f0102
jessica8709 <racheljessicaallu@gmail.com>
2023-10-15 00:26:41 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/jessica8709'
+      className='link footer__link'
+    >
+      Created By Rachel Jessica
+    </a>
+  </footer>
+)
+
+export default Footer
